945462b96fc2f383d5d36221fae64870184bc72c
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-04-09 08:02:33 -0500

    Update CarlosCardona_Ejercicio23.py

--- CarlosCardona_Ejercicio23.py
+++ CarlosCardona_Ejercicio23.py
@@ -4,5 +4,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-os.system("g++ Coeficientes.cpp -o coeficientes.x")
+os.system("g++ coeficientes.cpp -o coeficientes.x")
 os.system("./coeficientes.x")
